3e6a8a793f2a1c77cf3751df19b2eac15926b50c
javiperales <javiperales95@gmail.com>
2019-02-07 16:42:00 +0100

    trabajo por hacer

--- actividad 4/main.js
+++ actividad 4/main.js
@@ -1,31 +1,41 @@
 $(function () {
     console.log("entrada");
     cargarAjax();
-    console.log("Final");
-    
+    // console.log("Final");
 });
-
-var pintarMenu = function(respuesta){
-   
-    for(m of respuesta.menu){
-        document.write(m.denominacion+"<br>");
-        document.write(m.url+"<br>");
-    }
-}
 
 var cargarAjax = function () {
     $.ajax({
         url: "actividad4.json",
         dataType: "json"
+
     }).done(function (respuesta) {
-        console.log("Lectura ajax");
-        console.log(respuesta);
-        pintarMenu(respuesta);
-        
+
+        //console.log(respuesta);
+        pintarDatos(respuesta);
         console.log("final Lectura ajax");
     }).fail(function () {
         console.log("Fallo");
     }).always(function () {
-        document.write('<p>finalizado</p>');
+        //document.write("<p>Finalizando</p>");
     });
+}
+var pintarDatos = function (respuesta) {
+    var menu = document.getElementById("menu");
+    var ul1 = document.createElement("ul");
+    for (let m of respuesta.menu) {
+        document.write("<ul>")
+        document.write("<a href='#'>" + m.denominacion + "</a>" + "<br>")
+        document.write("</ul>")
+        for (let h of m.hijos) {
+
+            document.write("<li>")
+            document.write(h.denominacion);
+            document.write(h.url);
+            document.write("</li>")
+
+        }
+
+    }
+
 }
