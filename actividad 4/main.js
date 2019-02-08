61b34d3bc261785834c1f9f45141ce3dab8e5d30
javi perales <javiperales95@gmail.com>
2019-02-08 18:48:53 +0100

    terminar

--- actividad 4/main.js
+++ actividad 4/main.js
@@ -3,6 +3,7 @@ $(function () {
     cargarAjax();
     // console.log("Final");
 });
+var cadena=""; 
 
 var cargarAjax = function () {
     $.ajax({
@@ -11,44 +12,40 @@ var cargarAjax = function () {
 
     }).done(function (respuesta) {
 
-       
-        pintarDatos(respuesta);
-       
+
+        pintarDatos(respuesta.menu);
+
     }).fail(function () {
         console.log("Fallo");
     }).always(function () {
-       
+
     });
 }
-var pintarDatos = function (respuesta) {
-    var menu = document.getElementById("menu");
-    var ul1 = document.createElement("ul");
-    
-    for (let m of respuesta.menu) {
-        if(comprobarHijos(m.hijos)){
-            document.write("<ul>")
-        document.write("<a href='#'>" + m.denominacion + "</a>" + "<br>")
-        document.write("</ul>")
+var pintarDatos = function (menu) {
+     cadena+="<ul>";
+    for (let m of menu) {
+       cadena+="<li><a href='#'>"+ m.denominacion+ "</a></li>"
+        if (comprobarHijos(m.hijos)) {
+            pintarDatos(m.hijos)
+            
         }
         
-        
-//        for (let h of m.hijos) {
-//
-//            document.write("<li>")
-//            document.write(h.denominacion+"<br>");
-//            document.write("</li>")
-//            
-//
-//        }
 
-    }
 
-}
+       
 
-var comprobarHijos = function(hijo){
-    if(hijo=!undefined){
+    }//for
+    cadena+="</ul>"
+    $("#menu").html(cadena)
+    
+
+}//pintardatos
+
+var comprobarHijos = function (hijo) {
+    if (hijo != undefined) {
         return true;
-    }else{
+    } else {
         return false;
     }
 }
+
