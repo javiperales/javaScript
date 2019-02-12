475e6aace4724b3d2ac5a07336c34b7b9f24d5c3
javiperales <javiperales95@gmail.com>
2019-02-12 16:59:46 +0100

    Update main.js

--- actividad 4/main.js
+++ actividad 4/main.js
@@ -13,6 +13,7 @@ var cargarAjax = function () {
     }).done(function (respuesta) {
 
         pintarDatos(respuesta.menu, null);
+        desplegar();
 
     }).fail(function () {
         console.log("Fallo");
@@ -27,33 +28,39 @@ var pintarDatos = function (menu) {
     let ul = document.createElement("ul");
 
     let li = null;
+    
 
     for (let m of menu) {
         li = document.createElement("li");
-        li.setAttribute("class", "list li"+m.denominacion.replace(" ", "-"));
+        button = document.createElement("button");
+        button.setAttribute("class" , "click"+m.denominacion.replace(" ", "-"));
+        button.setAttribute("value", "->");
+        button.innerHTML="->";
+        li.setAttribute("class", "list li" + m.denominacion.replace(" ", "-"));
         let a = document.createElement("a");
-        a.setAttribute("href", "#");
-        a.setAttribute("class", "link");
+        //a.setAttribute("href", "#");
+        //a.setAttribute("class", "link");
 
         a.innerHTML = m.denominacion.replace(" ", "-");
+      
         li.append(a);
-        
+          li.append(button)
         if (comprobarHijos(m.hijos)) {
             let ulli = pintarDatos(m.hijos);
-            ulli.setAttribute("class","ul"+ m.denominacion.replace(" ", "-"));
+            ulli.setAttribute("class", "ul" + m.denominacion.replace(" ", "-"));
             li.appendChild(ulli);
-            
-             $(li).click(function () {
-            $(ulli).toggle("fast");
-                 console.log("clicar");
 
-        });
+            $(li).click(function () {
+                $(ulli).toggle("fast");
+                console.log("clicar");
+
+            });
 
         } else {
             // ul.append(li);
             main.append(ul);
         }
-       
+
 
         ul.appendChild(li);
         console.log("entro en for");
@@ -61,23 +68,6 @@ var pintarDatos = function (menu) {
 
     } //for
     return ul;
-} //pintardatos
-
-
-    cadena += "<ul>";
-   
-    for (let m of menu) {
-         mostrar();
-        cadena += "<li class='list'><a href='#'>" + m.denominacion + "</a></li>"
-        if (comprobarHijos(m.hijos)) {
-            pintarDatos(m.hijos)
-
-        }
-    } //for
-    cadena += "</ul>"
-    $("#menu").html(cadena)
-
-
 } //pintardatos
 
 
@@ -92,13 +82,11 @@ var comprobarHijos = function (hijo) {
 
 var desplegar = function () {
 
-    $(".list").click(function () {
-        $(".list ul").toggle("slow");
-    });
-
-var mostrar = function () {
-    $(".list").click(function () {
-        $(this).hide("fast");
-    })
-
+        $(".click"+m.denominacion.replace(" ", "-")).click(function () {
+            $(this).children().toggle("slow", function(){
+                $(this).stop();
+            });
+        });
 }
+
+
