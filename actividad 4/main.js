4b44a606682a4fd5469c75d80a66a33b2c5a10a0
javiperales <javiperales95@gmail.com>
2019-02-14 16:02:35 +0100

    hacer con jquery

--- actividad 4/main.js
+++ actividad 4/main.js
@@ -1,11 +1,7 @@
 $(function () {
     console.log("entrada");
     cargarAjax();
-    
-    // console.log("Final");
 });
-var cadena = "";
-
 var cargarAjax = function () {
     $.ajax({
         url: "actividad4.json",
@@ -14,10 +10,10 @@ var cargarAjax = function () {
     }).done(function (respuesta) {
 
         pintarDatos(respuesta.menu, null);
-       // desplegar();
-        $(" button ").click( (e) => {
+        // desplegar();
+        $(" button ").click((e) => {
             //console.log($($(e.target).parent().get(0)).children("ul")); 
-            $( $(e.target).parent().get(0) ).children("ul").toggle();
+            $($(e.target).parent().get(0)).children("ul").toggle();
         });
 
     }).fail(function () {
@@ -29,51 +25,41 @@ var cargarAjax = function () {
 var pintarDatos = function (menu) {
 
     var main = document.getElementById("menu");
-    let ul = document.createElement("ul");
-
-    let li = null;
     
-
+    let ul = document.createElement("ul");
+    let li = null;
     for (let m of menu) {
-                
+
         li = document.createElement("li");
         let button = document.createElement("button");
-        button.setAttribute("class" , "click"+m.denominacion.replace(" ", "-"));
+        button.setAttribute("class", "click" + m.denominacion.replace(" ", "-"));
         button.setAttribute("value", "->");
-        button.innerHTML="->";
+        button.innerHTML = "->";
         li.setAttribute("class", "list li" + m.denominacion.replace(" ", "-"));
         let a = document.createElement("a");
         a.setAttribute("href", m.url);
-        m.hasOwnProperty("target") ? a.setAttribute("target", m.target): null;
+        m.hasOwnProperty("target") ? a.setAttribute("target", m.target) : null;
         a.setAttribute("class", "link");
 
         a.innerHTML = m.denominacion.replace(" ", "-");
-      
+
         li.append(a);
-          li.append(button)
+        li.append(button)
         if (comprobarHijos(m.hijos)) {
             let ulli = pintarDatos(m.hijos);
             ulli.setAttribute("class", "ul" + m.denominacion.replace(" ", "-"));
             li.appendChild(ulli);
 
-           
-
         } else {
             // ul.append(li);
             main.append(ul);
         }
-        
-        
-        
+
         ul.appendChild(li);
         console.log("entro en for");
         main.append(ul);
 
     } //for
-    
-    
-
-    
     return ul;
 } //pintardatos
 
@@ -85,7 +71,3 @@ var comprobarHijos = function (hijo) {
         return false;
     }
 }
-
-
-
-
