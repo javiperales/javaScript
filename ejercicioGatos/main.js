4f934d91d92966f1068fb47069011e9fff5381cb
javi perales <javiperales95@gmail.com>
2019-04-25 02:22:14 +0200

    ejerciciogatos

--- ejercicioGatos/main.js
+++ ejercicioGatos/main.js
@@ -1,10 +1,14 @@
-const allcat = new Set();
-const allsex = new Set();
-const allraza = new Set();
-var obj;
+const sexoGato = new Set();
+const gatoCastrado = new Set();
+const gatoRaza = new Set();
+var objeto;
 
 window.onload = function () {
     cargarAjax();
+}
+
+Array.prototype.unique = function () {
+    return this.filter((x, i, a) => a.indexOf(x, i + 1) < 0);
 }
 
 function cargarAjax() {
@@ -18,12 +22,12 @@ function cargarAjax() {
     function mostrar() {
         console.log('estamos en mostrar');
         if (http.readyState == 4 && http.status == 200) {
-
-
-            obj = JSON.parse(this.responseText);
-            mostrarDatos(obj);
-            cargarSelect(obj);
-            cargarGatos(obj);
+            objeto = JSON.parse(this.responseText);
+            mostrarDatos();
+            mostrarDatosFiltrar();
+            rellenarSelects();
+            filtrarPorSexo();
+            detectarEnter();
         }
     }
 
@@ -31,11 +35,113 @@ function cargarAjax() {
     console.log('salimos mostrar');
 }
 
-function mostrarDatos(){
-    var divContenedor = document.getElementById("contenedor");
+function mostrarDatos() {
+    //console.log(objeto)
+    var resultado = document.getElementById("contenedor");
+    for (gatos in objeto) {
+        for (datos of objeto[gatos]) {
+            var ul = document.createElement("ul");
+            for (gato in datos) {
+                var li = document.createElement("li");
+                if (gato == "ruta") {
+                    var imagen = document.createElement("img");
+                    imagen.src = datos[gato];
+                    li.append(imagen);
+                } else {
+                    li.innerHTML = datos[gato];
 
-    for(gatos of obj.gatos.json){
-        var divGatos = document.createElement("div");
-        //empezar recorrido de los gatos, y filtrar los gatos
+                }
+                ul.append(li)
+            }
+            resultado.append(ul);
+        }
     }
+
+
+}
+
+function mostrarDatosFiltrar(){
+   
+    for(gatos in objeto){
+        for(datos of objeto[gatos]){
+            for(gato in datos){
+               if(gato == "sexo"){
+                  sexoGato.add(datos[gato]);
+                  console.log(sexoGato);
+               }
+
+               if(gato=="raza"){
+                gatoRaza.add(datos[gato]);
+               }
+
+               if(gato=="castrado"){
+                gatoCastrado.add(datos[gato]);
+               }
+
+            }
+        }
+    }
+}
+
+function rellenarSelects(){
+    var selectSexo= document.getElementById("sexo");   
+    var selectRaza= document.getElementById("raza");   
+    var selectCastrado= document.getElementById("castrado");   
+     
+    for(sexo of sexoGato){
+        var opc= document.createElement("option");
+        opc.innerHTML=sexo;
+        selectSexo.append(opc);
+    }
+
+    for(raza of gatoRaza){
+        var opc= document.createElement("option");
+        opc.innerHTML=raza;
+        selectRaza.append(opc);
+    }
+
+    for(castrado of gatoCastrado){
+        var opc= document.createElement("option");
+        opc.innerHTML=castrado;
+        selectCastrado.append(opc);
+    }
+}
+
+function filtrarPorSexo(){
+    var resultado = document.getElementById("contenedor");
+    var opcionSeleccionada = document.getElementById("sexo");
+    opcionSeleccionada.addEventListener("change", function(){
+        var sexo = this.options[opcionSeleccionada.selectedIndex];
+            if(objeto.gatos.filter(a => a.sexo.includes(sexo.value))){
+                objeto = (objeto.gatos.filter(a=>a.sexo.includes(sexo.value)))
+            }
+            console.log(objeto);
+            document.getElementById("contenedor").innerHTML = "";
+
+            for(dato of objeto){
+                var ul = document.createElement("ul");
+                for(gato in dato){
+                   var li = document.createElement("li");
+                   if(gato=="ruta"){
+                    var imagen = document.createElement("img");
+                    imagen.src= dato[objeto];
+                    li.append(imagen);
+                   }else{
+                       li.innerHTML=dato[gato];
+                   }
+                   ul.append(li);
+                }
+                resultado.append(ul);
+            }
+           
+    });
+}
+
+function detectarEnter(e){
+    var enter= addEventListener("keypress", function(e){
+        if(e.keyCode==13){
+            var nombre = document.getElementsByTagName("input")[0].value;
+           
+        }
+    })
 }
